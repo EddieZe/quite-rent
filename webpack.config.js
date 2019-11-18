b5031dab30656a756bb2d0314a7cfec49ac985d5
Eddie <eddie.zeltser@gmail.com>
2019-11-18 12:57:38 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: "./src/index.tsx"
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html"
+        })
+    ],
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"],
+                exclude: /node_modules/
+            }]
+    }
+};
